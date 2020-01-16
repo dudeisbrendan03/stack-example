b8b91b0fe62e97bbbacb536ebe6be5d625d8abfd
Brendan Jennings <jbrendan70@outlook.com>
2020-01-16 20:20:54 +0000

    Update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     name="stack-module",
 
     # Version number (initial):
-    version="0.1.1",
+    version="0.1.2",
 
     # Application author details:
     author="Brendan T D. Jennings",
